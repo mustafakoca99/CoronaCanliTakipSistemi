f683e64bb088eb2c4c73c2cd28b60deadc6a3251
mustafakoca220@gmail.com <mustafakoca220@gmail.com>
2020-05-21 23:44:43 +0300

    Add project files.

---
+++ CoronaCanliTakipSistemi/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace CoronaCanliTakipSistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TÜRKİYE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ABD";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "İTALYA";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 200);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "İSPANYA";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(127, 41);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(208, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Türkiye İçin Bugünün Verilerini Kaydet!";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(127, 91);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(195, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "ABD İçin Bugünün Verilerini Kaydet!";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(127, 143);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(198, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "İtalya İçin Bugünün Verilerini Kaydet!";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(127, 200);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(210, 17);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "İspanya İçin Bugünün Verilerini Kaydet!";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 275);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Hasta";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Ölüm";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(418, 185);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(127, 59);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(116, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Tabloda Göster-TR";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(249, 59);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(130, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Tabloda Gösterme-TR";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(127, 108);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(123, 17);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Tabloda Göster-ABD";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(249, 108);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(137, 17);
+            this.radioButton4.TabIndex = 12;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Tabloda Gösterme-ABD";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(127, 164);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(111, 17);
+            this.radioButton5.TabIndex = 13;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Tabloda Göster-İT";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(249, 164);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(111, 17);
+            this.radioButton6.TabIndex = 14;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Tabloda Göster-İT";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(127, 224);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(115, 17);
+            this.radioButton7.TabIndex = 15;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Tabloda Göster-ES";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(249, 224);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(115, 17);
+            this.radioButton8.TabIndex = 16;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Tabloda Göster-ES";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 245);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(417, 23);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "HER İŞLEMDEN SONRA SAYFAYI YENİLEMEK İÇİN TIKLA!";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(421, 15);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "SAYFA YENİLEME BUTONUNA BASTIKTAN SONRA BİRAZ BEKLEYİN!";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(441, 472);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "KORONA TAKİP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CoronaCanliTakipSistemi/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace CoronaCanliTakipSistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //global tanımladık
+        XElement root;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //internetten veriyi aldık
+            XDocument doc = XDocument.Load("https://opendata.ecdc.europa.eu/covid19/casedistribution/xml");
+            root = doc.Root;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //türkiye için verileri al
+            if (checkBox1.Checked==true)
+            {
+                //anlık olarak bilgi almak için kullanılan kod
+                List<XElement> xusabugun = root.Elements()
+                .Where(s => s.Element("geoId").Value == "TR" && 
+                DateTime.Parse(s.Element("dateRep").Value).Date == DateTime.Now.Date)
+                .ToList();
+                XDocument newDocTR = XDocument.Parse("<root></root>");
+                XElement newRootTR = newDocTR.Root;
+                newRootTR.Add(xusabugun);
+                newRootTR.Save("Turkiye.xml");
+                MessageBox.Show("Türkiye verileri kaydedildi!", "BAŞARILI!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (radioButton1.Checked == true)
+            {
+                //tr değerini çektik
+                var trDegerler = root.Elements().Where(s => s.Element("geoId").Value == "TR");
+                //tr zamanları tersten sıralıyor
+                var sirali = trDegerler.OrderByDescending(s => DateTime.Parse(s.Element("dateRep").Value));
+                //son 5 zamanı alıyor
+                var son5 = sirali.Take(5);
+                //son 5 olanı chart tablosuna ekliyoruz
+                foreach (var item in son5)
+                {
+                    chart1.Series["Hasta"].Points
+                        .AddXY(item.Element("dateRep").Value, int.Parse(item.Element("cases").Value));
+
+                    chart1.Series["Ölüm"].Points
+                       .AddXY(item.Element("dateRep").Value, int.Parse(item.Element("deaths").Value));
+                }
+            }
+            else if (radioButton2.Checked==true)
+            {
+                MessageBox.Show("Türkiye verileri grafik ile gösterilmeyecek!","GRAFİK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Türkiye verileri kaydedilemedi!", "HATA!",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //amerika için verileri al
+            if (checkBox2.Checked==true)
+            {
+                //anlık olarak bilgi almak için kullanılan kod
+                List<XElement> xusabugun = root.Elements()
+                .Where(s => s.Element("geoId").Value == "US" && 
+                DateTime.Parse(s.Element("dateRep").Value).Date == DateTime.Now.Date)
+                .ToList();
+                XDocument newDocUsa = XDocument.Parse("<root></root>");
+                XElement newRootUsa = newDocUsa.Root;
+                newRootUsa.Add(xusabugun);
+                newRootUsa.Save("ABD.xml");
+                MessageBox.Show("ABD verileri kaydedildi!", "BAŞARILI!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (radioButton3.Checked==true)
+            {
+                //ABD değerini çektik
+                var trDegerler = root.Elements().Where(s => s.Element("geoId").Value == "US");
+                //ABD zamanları tersten sıralıyor
+                var sirali = trDegerler.OrderByDescending(s => DateTime.Parse(s.Element("dateRep").Value));
+                //son 5 zamanı alıyor
+                var son5 = sirali.Take(5);
+                //son 5 olanı chart tablosuna ekliyoruz
+                foreach (var item in son5)
+                {
+                    chart1.Series["Hasta"].Points
+                        .AddXY(item.Element("dateRep").Value, int.Parse(item.Element("cases").Value));
+
+                    chart1.Series["Ölüm"].Points
+                       .AddXY(item.Element("dateRep").Value, int.Parse(item.Element("deaths").Value));
+                }
+            }
+            else if (radioButton4.Checked==true)
+            {
+                MessageBox.Show("ABD verileri grafik ile gösterilmeyecek!", "GRAFİK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("ABD verileri kaydedilemedi!", "HATA!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //italya için verileri al
+            if (checkBox3.Checked==true)
+            {
+                //anlık olarak bilgi almak için kullanılan kod
+                List<XElement> xusabugun = root.Elements()
+               .Where(s => s.Element("geoId").Value == "IT" &&
+               DateTime.Parse(s.Element("dateRep").Value).Date == DateTime.Now.Date)
+               .ToList();
+                XDocument newDocIT = XDocument.Parse("<root></root>");
+                XElement newRootIT = newDocIT.Root;
+                newRootIT.Add(xusabugun);
+                newRootIT.Save("Italya.xml");
+                MessageBox.Show("İtalya verileri kaydedildi!", "BAŞARILI!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (radioButton5.Checked==true)
+            {
+                var trDegerler = root.Elements().Where(s => s.Element("geoId").Value == "IT");
+                //IT zamanları tersten sıralıyor
+                var sirali = trDegerler.OrderByDescending(s => DateTime.Parse(s.Element("dateRep").Value));
+                //son 5 zamanı alıyor
+                var son5 = sirali.Take(5);
+                //son 5 olanı chart tablosuna ekliyoruz
+                foreach (var item in son5)
+                {
+                    chart1.Series["Hasta"].Points
+                        .AddXY(item.Element("dateRep").Value, int.Parse(item.Element("cases").Value));
+
+                    chart1.Series["Ölüm"].Points
+                       .AddXY(item.Element("dateRep").Value, int.Parse(item.Element("deaths").Value));
+                }
+            }
+            else if (radioButton6.Checked==true)
+            {
+                MessageBox.Show("İtalya verileri grafik ile gösterilmeyecek!", "GRAFİK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("İtalya verileri kaydedilemedi!", "HATA!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //İspanya için verileri al
+            if (checkBox3.Checked == true)
+            {
+                //anlık olarak bilgi almak için kullanılan kod
+                List<XElement> xusabugun = root.Elements()
+               .Where(s => s.Element("geoId").Value == "ES" &&
+               DateTime.Parse(s.Element("dateRep").Value).Date == DateTime.Now.Date)
+               .ToList();
+                XDocument newDocES = XDocument.Parse("<root></root>");
+                XElement newRootES = newDocES.Root;
+                newRootES.Add(xusabugun);
+                newRootES.Save("Ispanya.xml");
+                MessageBox.Show("İspanya verileri kaydedildi!", "BAŞARILI!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (radioButton7.Checked==true)
+            {
+                var trDegerler = root.Elements().Where(s => s.Element("geoId").Value == "ES");
+                //ES zamanları tersten sıralıyor
+                var sirali = trDegerler.OrderByDescending(s => DateTime.Parse(s.Element("dateRep").Value));
+                //son 5 zamanı alıyor
+                var son5 = sirali.Take(5);
+                //son 5 olanı chart tablosuna ekliyoruz
+                foreach (var item in son5)
+                {
+                    chart1.Series["Hasta"].Points
+                        .AddXY(item.Element("dateRep").Value, int.Parse(item.Element("cases").Value));
+
+                    chart1.Series["Ölüm"].Points
+                       .AddXY(item.Element("dateRep").Value, int.Parse(item.Element("deaths").Value));
+                }
+            }
+            else if (radioButton8.Checked==true)
+            {
+                MessageBox.Show("İspanya verileri grafik ile gösterilmeyecek!", "GRAFİK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("İspanya verileri Kaydedilemedi!", "HATA!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            frm.Show();
+            this.Hide();
+        }
+    }
+}
